7e9c48e0f1f3a900128ce9c8510360750a5399c2
taidao1901 <19522148@gm.uit.edu.vn>
2022-09-24 10:51:15 +0700

    schema

--- Test.py
+++ Test.py
@@ -1,44 +1,17 @@
-# import requests
-# import http.cookiejar 
-# import pickle
-# from fake_useragent import UserAgent
-# import browser_cookie3
-# ua = UserAgent()
-# header = {
-#     'User-Agent':str(ua.chrome),
-#     'x-Requested-With': 'XMLHttpRequest'
-# }
-# cj = http.cookiejar.MozillaCookieJar('shopee.vn_cookies.txt')
-# cj.load()
-# def save_cookies(requests_cookiejar, filename):
-#     with open(filename, 'wb') as f:
-#         pickle.dump(requests_cookiejar, f)
-
-# def load_cookies(filename):
-#     with open(filename, 'rb') as f:
-#         return pickle.load(f)
-
-# #save cookies
-# session = requests.Session()
-
-# # cj = browser_cookie3.chrome(domain_name='shopee.vn')
-# # r = session.get('https://shopee.vn/product/3064018/399188377/')
-# # cookies= r.cookies
-# # print(r.cookies.get_dict())
-# # #load cookies and do a request
-
-# # for cookie in cj:
-# #    print (cookie.name, cookie.value, cookie.domain)
-# for cookie in cj:
-#    print (cookie.name, cookie.value, cookie.domain)
-# url ='https://shopee.vn/api/v4/item/get?itemid=399188377&shopid=3064018'
-# res= session.get( url ,headers = header, cookies=cj)
-# print(res.content)
+import json
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
-driver.get("https://shopee.vn/Th%E1%BB%9Di-Trang-Nam-cat.11035567")
-print(driver.get_cookies())
-driver.get("https://shopee.vn/api/v4/item/get?itemid=399188377&shopid=3064018")
+caps = DesiredCapabilities.CHROME
+caps['goog:loggingPrefs'] = {'performance': 'ALL'}
+driver = webdriver.Chrome(desired_capabilities=caps)
+driver.get('https://stackoverflow.com/questions/52633697/selenium-python-how-to-capture-network-traffics-response')
+
+def process_browser_log_entry(entry):
+    response = json.loads(entry['message'])['message']
+    return response
+
+browser_log = driver.get_log('performance') 
+events = [process_browser_log_entry(entry) for entry in browser_log]
+events = [event for event in events if 'Network.response' in event['method']]
+print(type(events))
