62be236003d88d81a91268e585ef2e1c9047a53d
taidao1901 <19522148@gm.uit.edu.vn>
2022-09-28 23:18:03 +0700

    final version

--- Test.py
+++ Test.py
@@ -1,5 +1,5 @@
 import json
-from selenium import webdriver
+from sqlite3 import Row
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
@@ -18,7 +18,8 @@ from random_user_agent.params import SoftwareName, OperatingSystem
 from seleniumwire import webdriver
 from HelpTools.ReadCategories import Categories
 from TinProxyService import GetProxyIP
-
+import logging
+import pandas as pd 
 
 def CreateService():
     service = Service(executable_path=ChromeDriverManager().install())
@@ -60,7 +61,7 @@ def CreateService():
     return driver
 
 def GetItems(url:str, page: int, patient = 0) -> dict:
-    
+
     def process_browser_log_entry(entry):
         response = json.loads(entry['message'])['message']
         return response
@@ -73,48 +74,47 @@ def GetItems(url:str, page: int, patient = 0) -> dict:
         time.sleep(3)
         driver.get(f"{url}?page={page}")
         time.sleep(10)
-        browser_log = driver.get_log('performance') 
-        events = [process_browser_log_entry(entry) for entry in browser_log]
-        events = [event for event in events if 'Network.response' in event['method']]
+        browser_log = driver.get_log('performance')
+        for entry in browser_log:
+            event = process_browser_log_entry(entry)
+            try:
+                if  "https://shopee.vn/api/v4/search/search_items?by=relevancy" in event["params"]["response"]["url"]:
+                    response = driver.execute_cdp_cmd('Network.getResponseBody', {'requestId': event["params"]["requestId"]})
+                    items = (json.loads(response['body'])["items"])
+                    break
+            except:
+                pass
+        # events = [process_browser_log_entry(entry) for entry in browser_log]
+        # events = [event for event in events if 'Network.response' in event['method']]
     # except Exception() as e:
     except:
         driver.close()
         print(f"###################Không vào được links page {page}. Tải lại...")
         return GetItems(url, page, patient + 1)
-        # print(e)
-        # return items
-    for i in range(len(events)):
-        try:
-            if  "https://shopee.vn/api/v4/search/search_items?by=relevancy" in events[i]["params"]["response"]["url"]:
-                response = driver.execute_cdp_cmd('Network.getResponseBody', {'requestId': events[i]["params"]["requestId"]})
-                items = (json.loads(response['body'])["items"])
-        except:
-            pass
+
     driver.execute_script("window.stop();")
-    return items
+    return items, page
 
 def GetProductsDetails(items: list,customerCategoryId: str) -> list:
     result = []
-    if items is not None:
+    if len(items)>0:
         for item in items:
             try:
-                item =item["item_basic"]
-                productSlug = item['name'].replace(" ","-")
+                item = item["item_basic"]
                 result.append(
                     {
                         'ItemId': item['itemid'],
                         "ShopId": item["shopid"],
                         'Name': item['name'],
                         'Images': [f"https://cf.shopee.vn/file/{image}" for image in item['images']],
-                        'ProductURL': f"https://shopee.vn/{productSlug}-i.{item['shopid']}.{item['itemid']}",
+                        'ProductURL': f"https://shopee.vn/product/{item['shopid']}/{item['itemid']}",
                         'CustomerCategoryId':customerCategoryId,
                         "SellerId":  item['catid'],
                         'LabelIds': item['label_ids'],
                         'Brand': item['brand'],
                         'Price': item['price'] if item['raw_discount'] == 0 else item['price_before_discount'],
                         'IsOfficialShop': item['is_official_shop'],
-                        'FetchedTime': datetime.timestamp(datetime.utcnow()),
-                        # 'Description':''
+                        'FetchedTime': datetime.now().timestamp(),
                     }
                 )
             except Exception as e:
@@ -122,28 +122,45 @@ def GetProductsDetails(items: list,customerCategoryId: str) -> list:
                 pass
     return result
 
-def CrawlByCategory(url, pageQuantity: int, maxWorkers=8) -> list:
+def CrawlByCategory(url, pageRange = (0,20), maxWorkers=8) -> list:
     futures =[]
     items = []
+    pages = []
+    log = {"CategoryLink": url}
     with concurrent.futures.ThreadPoolExecutor(max_workers= maxWorkers) as executor:
-        for page in range(pageQuantity):
+        for page in range(pageRange[0],pageRange[1]):
             futures.append(executor.submit(GetItems,url,page))
     customerCategoryId = url.split(".")[-1]
     for future in concurrent.futures.as_completed(futures):
-        items.extend(future.result())
-    
+        res, page = future.result()
+        if len(res)>0:
+            pages.append(page)
+            items.extend(res)
+    log["IsCompelete"] = 1 if len(items)>=2000 else 0
+    log["ProductQuantity"] = len(items)
+    log["PagesAreCrawled"] = pages
+    log["CrawlTime"] = datetime.now()
     result = GetProductsDetails(items, customerCategoryId)
-    return result
-
+    return result,log
+def SaveLog(log,filePath =r"tmp\CrawlByCategory.csv"):
+    logs = pd.read_csv(filePath)
+    try:
+        logs= logs.drop(logs[logs.CategoryLink == log["CategoryLink"]].index)
+    except: 
+        pass
+    logs = pd.concat([logs,pd.DataFrame([log])],ignore_index=True,axis=0)
+    logs.to_csv(filePath, index= False)
 if __name__=="__main__":
     # https://httpbin.org/ip
     # result = CrawlByCategory("https://httpbin.org/ip", 16)
+    logging.basicConfig(filename=r'tmp\CrawlByCategory.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s',datefmt='%H:%M:%S', level=logging.DEBUG)
     categories = Categories("Categories.json")
     allPaths =categories.GetAllPaths()
     # print(allPaths)
-    result = CrawlByCategory(allPaths[0], 32)
+    result,log = CrawlByCategory(allPaths[0], (0,1))
+    SaveLog(log)
     # print(result)
-    # with open('data.json', 'w', encoding='utf-8') as f:
-    #     json.dump(result, f, ensure_ascii=False, indent=4)
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(result, f, ensure_ascii=False, indent=4)
     # print(GetProxyIP.LoadApiKeyAndAllowIp())
     # print(GetProxyIP.GetProxyIps())
