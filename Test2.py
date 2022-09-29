c45efc9aaa698c0f2f4a64541b7b0fa41a3b85aa
taidao1901 <19522148@gm.uit.edu.vn>
2022-09-29 17:25:28 +0700

    test2

--- Test2.py
+++ Test2.py
@@ -1,14 +1,20 @@
-import requests
-import selenium
-import seleniumwire
+import json
 
-if __name__=="__main__":
-    # token = ""
-    # url = f"https://api.tinproxy.com/user/renew-proxy?token={token}"
-    # data = {"api_key": "MrIVnojEv4OqdBDTZB2BFnzLdah0IaOz", "time": 1}
-    # r = requests.post(url=url, data=data)
-    # result = r.json()
-    # print(result, r.status_code)
-    # print(selenium.__version__)
-    # print(seleniumwire.__version__)
-    pass
+with open('data.json', 'r+', encoding='utf-8') as f:
+    data = json.loads(f.read())
+    # f.seek(0)
+    # f.truncate()
+    # data.extend(
+    #     [
+    #         {
+    #             "a" :"asff"
+    #         }
+    #     ]
+    # )
+    # json.dump(data, f, ensure_ascii=False, indent=4)
+for item in data:
+    # if "CustomerCategoryId" not in item.keys():
+    #     print(item) 
+    if item["CustomerCategoryId"] not in ["11035571","11035570"]:
+        print(item["ItemId"]) 
+print(type(data))
